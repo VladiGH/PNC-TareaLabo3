088b2f892e61ea020d18e4e013ad9b051807a62f
VladiGH <00103017@uca.edu.sv>
2020-05-04 23:07:51 -0600

    tarea labo 3 falta concatenar

---
+++ tareaLabo3/src/main/java/com/uca/capas/tareaLabo3/controller/MainController.java
@@ -0,0 +1,45 @@
+package com.uca.capas.tareaLabo3.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.tareaLabo3.domain.Product;
+
+@Controller
+public class MainController {
+	private List<Product> productList = new ArrayList<Product>();
+	
+	@GetMapping("/compraProducto")
+	public ModelAndView compra() {
+		ModelAndView mav = new ModelAndView();
+		
+		productList.add(new Product(0,"100 Años de Soledad", "50"));
+		productList.add(new Product(1, "Rayuela","25"));
+		productList.add(new Product(2, "La Casa de los espíritus","35"));
+		productList.add(new Product(3, "Divina Comedia", "50"));
+		productList.add(new Product(4, "Ensayo sobre la ceguera","75"));
+		
+		mav.setViewName("index");
+		mav.addObject("product", new Product());
+		mav.addObject("productos", productList);
+		return mav;
+	}
+	
+	@PostMapping("/validar")
+	public ModelAndView validar(Product producto) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("nombre", producto.getNombre());
+		int cantidadProdLista = Integer.parseInt(productList.get(producto.getId()).getCantidad());
+		int cantidadProd = Integer.parseInt(producto.getCantidad());
+		if (cantidadProd <= cantidadProdLista && cantidadProd > 0) 
+			mav.setViewName("compra");
+		else 
+			mav.setViewName("error");
+		return mav;
+	}
+}
---
+++ tareaLabo3/src/main/java/com/uca/capas/tareaLabo3/domain/Product.java
@@ -0,0 +1,39 @@
+package com.uca.capas.tareaLabo3.domain;
+
+public class Product {
+	int id;
+	String nombre;
+	String cantidad;
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Product(int id, String nombre, String cantidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+}
